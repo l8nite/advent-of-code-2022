879c56b0b74b4da7d6f9527e527342d101d97514
Shaun Guth <shaun.guth@gmail.com>
2022-12-01 22:24:02 -0800

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
+    fun parseElves(input: List<String>): List<Int> {
+        val elves = mutableListOf<Int>()
+        var total = 0
+
+        input.forEach { calories ->
+            if (calories.isBlank()) {
+                elves.add(total)
+                total = 0
+            } else {
+                total += calories.toInt()
+            }
+        }
+
+        return elves
+    }
+
+    // find the highest-calorie elf
     fun part1(input: List<String>): Int {
-        return input.size
+        return parseElves(input).maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseElves(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+     val input = readInput("Day01")
+     println(part1(input))
+     println(part2(input))
 }
