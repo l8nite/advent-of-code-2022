f08c954e88f132d97b70974ba4f6b0cc8fbee3c8
Shaun Guth <shaun.guth@gmail.com>
2022-12-01 22:33:08 -0800

    Fix issue with last elf in the file

--- src/Day01.kt
+++ src/Day01.kt
@@ -12,6 +12,8 @@ fun main() {
             }
         }
 
+        elves.add(total)
+
         return elves
     }
 
@@ -27,6 +29,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
      val input = readInput("Day01")
      println(part1(input))
