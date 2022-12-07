ae43b8d7a55510eb9ca7474ea3966f3025ba801a
Shaun Guth <shaun.guth@gmail.com>
2022-12-07 11:24:47 -0800

    TIL about Iterable.indexOfFirst()

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,10 +1,8 @@
 fun main() {
     fun detectSOT(signal: String, size: Int = 4): Int {
-        signal.windowed(size, step=1).forEachIndexed { index, chunk ->
-            if (chunk.toSet().size == size) { return index + size }
-        }
-
-        return -1
+        return signal.windowed(size, step=1).indexOfFirst {
+            it.toSet().size == size
+        } + size
     }
 
     fun part1(input: List<String>): Int {
