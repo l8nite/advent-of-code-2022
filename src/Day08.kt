724e477de7f167e7fa0bca217a00bbbbd53fc50e
Shaun Guth <shaun.guth@gmail.com>
2022-12-08 15:30:44 -0800

    Ok I'm happier with this solution now.

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,74 +1,30 @@
-import kotlin.math.abs
-
-enum class Direction(val v: Pair<Int,Int>) { NORTH(Pair(0, -1)), SOUTH(Pair(0, +1)), EAST(Pair(+1, 0)), WEST(Pair(-1, 0)) }
-
 fun main() {
-    fun visibility(forest: List<List<Int>>, x: Int, y: Int): Boolean? {
-        return Direction.values().firstNotNullOfOrNull {
-            var cx = x
-            var cy = y
-            var visible = true
+    fun getForest(input: List<String>) = input.map { r -> r.map { it.digitToInt() } }
+    fun trees(forest: List<List<Int>>) = forest.flatMapIndexed { y, r -> r.mapIndexed { x, h -> Triple(x, y, h) } }
 
-            while (
-                (it == Direction.NORTH && cy != 0) ||
-                (it == Direction.SOUTH && cy != forest.lastIndex) ||
-                (it == Direction.WEST && cx != 0) ||
-                (it == Direction.EAST && cx != forest[y].lastIndex)
-            ) {
-                cx += it.v.first
-                cy += it.v.second
+    // the 4 top-level sequences the 4 directions we can travel (left, right, up, down), the inner sequence is the
+    // height of each tree in that direction from the current point to the edge
+    fun lineOfSight(forest: List<List<Int>>, x: Int, y: Int) = listOf(
+        listOf((x - 1 downTo 0), (x + 1 until forest[y].size)).map { it.map { x -> x to y } },
+        listOf((y - 1 downTo 0), (y + 1 until forest.size)).map { it.map { y -> x to y } }
+    ).flatten().map { it.map { (x, y) -> forest[y][x] } }
 
-                if (forest[cy][cx] >= forest[y][x]) {
-                    visible = false
-                    break
-                }
-            }
-
-            if (!visible) null else true
-        }
+    fun viewingDistance(los: List<Int>, height: Int) = los.indexOfFirst { it >= height }.let {
+        if (it == -1) los.size else it + 1 // -1 means we hit the edge, otherwise add 1 to index for the distance
     }
 
     fun part1(input: List<String>): Int {
-        val forest = input.map { row -> row.map { it.digitToInt() } }
-        var visible = 0
-        forest.indices.forEach { y ->
-            forest[y].indices.forEach { x ->
-                if (visibility(forest, x, y) == true) { visible++ }
-            }
+        val forest = getForest(input)
+        return trees(forest).count { (x, y, h) ->
+            lineOfSight(forest, x, y).any { it.all { th -> th < h } }
         }
-
-        return visible
-    }
-
-    fun scenic(forest: List<List<Int>>, x: Int, y: Int): Int {
-        return Direction.values().map {
-            // count the number of trees of height less than ours in this direction
-            var cx = x
-            var cy = y
-            while (
-                (it == Direction.NORTH && cy != 0) ||
-                (it == Direction.SOUTH && cy != forest.lastIndex) ||
-                (it == Direction.WEST && cx != 0) ||
-                (it == Direction.EAST && cx != forest[y].lastIndex)
-            ) {
-                cx += it.v.first
-                cy += it.v.second
-                if (forest[cy][cx] >= forest[y][x]) {
-                    break
-                }
-            }
-
-            abs(x-cx) + abs(y-cy)
-        }.reduce(Int::times)
     }
 
     fun part2(input: List<String>): Int {
-        val forest = input.map { row -> row.map { it.digitToInt() } }
-        return forest.flatMapIndexed { y, row ->
-            row.indices.map { x ->
-                scenic(forest, x, y)
-            }
-        }.max()
+        val forest = getForest(input)
+        return trees(forest).maxOf { (x, y, h) ->
+            lineOfSight(forest, x, y).map { viewingDistance(it, h)}.reduce(Int::times)
+        }
     }
 
     // test if implementation meets criteria from the description, like:
