3ebd50469398f9935897923952397ffc8e1664be
Shaun Guth <shaun.guth@gmail.com>
2022-12-15 20:18:57 -0800

    An attempt at improving execution time by using IntRanges everywhere and no classes, but it didn't help much.

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,92 +1,89 @@
 import kotlin.math.abs
 import kotlin.math.max
-import kotlin.system.measureTimeMillis
 
-class Sensor(val x: Int, val y: Int, val beacon: Beacon, var c: Char = 'S') {
-    private val distance = abs(x - beacon.x) + abs(y - beacon.y)
+class MutableIntRange(var first: Int, var last: Int)
 
-    val minX = x - distance
-    val maxX = x + distance
-    val minY = y - distance
-    val maxY = y + distance
+fun List<IntRange>.combine(): List<IntRange> {
+    val result = mutableListOf<IntRange>()
+    var ranger: MutableIntRange? = null
 
-    val xRanges: Map<Int,IntRange> = ((y-distance)..(y+distance)).associateWith {
-        (x - distance + abs(y - it))..(x + distance - abs(y - it))
+    this.sortedBy { it.first }.forEachIndexed { idx, range ->
+        if (ranger == null) { ranger = MutableIntRange(range.first, range.last)}
+
+        if (range.first <= (ranger!!.last+1)) {
+            ranger!!.last = max(ranger!!.last, range.last)
+        } else {
+            result.add(IntRange(ranger!!.first, ranger!!.last))
+            ranger!!.first = range.first
+            ranger!!.last = range.last
+        }
     }
 
-    fun inRange(tx: Int, ty: Int): Boolean {
-        return xRanges[ty]?.contains(tx) ?: false
+    if (ranger != null) {
+        result.add(IntRange(ranger!!.first, ranger!!.last))
     }
+
+    return result
 }
-class Beacon(val x: Int, val y: Int)
+
 fun main() {
-    fun parse(input: String): Sensor {
-        val r = "^Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".toRegex()
-        val (sx, sy, bx, by) = r.matchEntire(input)!!.destructured
-        return Sensor(sx.toInt(), sy.toInt(), Beacon(bx.toInt(), by.toInt()), '@')
+    val sensorRegex = "^Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".toRegex()
+
+    fun parse(input: List<String>): Map<Int, List<IntRange>> {
+        val map = mutableMapOf<Int, MutableList<IntRange>>()
+
+        input.map {
+            sensorRegex.matchEntire(it)!!.destructured.toList().map(String::toInt)
+        }.forEach { (sx, sy, bx, by) ->
+            val d = abs(sx - bx) + abs(sy - by)
+            val yRange = ((sy - d)..(sy + d))
+            yRange.forEach{ y ->
+                map.getOrPut(y) { mutableListOf() }.add(
+                    (sx - d + abs(sy - y))..(sx + d - abs(sy - y))
+                )
+            }
+        }
+
+        map.replaceAll { y, ranges ->
+            ranges.combine() as MutableList<IntRange>
+        }
+
+        return map
     }
 
-    fun draw(sensors: List<Sensor>, highlight: Sensor? = null, rowY: Int? = null) {
-        val minX = highlight?.minX ?: sensors.minOf { it.minX }
-        val maxX = highlight?.maxX ?: sensors.maxOf { it.maxX }
-        val minY = highlight?.minY ?: sensors.minOf { it.minY }
-        val maxY = highlight?.maxY ?: sensors.maxOf { it.maxY }
+    fun draw(map: Map<Int, List<IntRange>>, sx: Int? = null, sy: Int? = null) {
+        val xMin = map.minOf { it.value.minOf { range -> range.first } }
+        val xMax = map.maxOf { it.value.maxOf { range -> range.last } }
 
-        for (y in minY..maxY) {
-            if (rowY != null && (y < rowY - 1 || y > rowY + 1)) {
-                continue
-            }
-
-            for (x in minX..maxX) {
-                if (sensors.any { it.x == x && it.y == y }) {
-                    print("S")
+        for (y in map.keys.sorted()) {
+            for (x in xMin..xMax) {
+                var c = '.'
+                if (map[y]!!.any { it.contains(x) }) {
+                    c = '#'
                 }
-                else if (sensors.any { it.beacon.x == x && it.beacon.y == y }) {
-                    print("B")
+                if (sx != null && x in 0..sx && (y == 0 || y == sy)) {
+                    c = '+'
                 }
-                else if (highlight == null && sensors.any { it.inRange(x, y) }) {
-                    print("#")
+                if (sy != null && y in 0..sy && (x == 0 || x == sx)) {
+                    c = '|'
                 }
-                else if (highlight != null && highlight.inRange(x, y)) {
-                    print("#")
-                }
-                else {
-                    print(".")
-                }
+                print(c)
             }
             print("\n")
         }
     }
 
     fun part1(input: List<String>, y: Int): Int {
-        val sensors = input.map { parse(it) }
-        val minX = sensors.minOf { it.minX }
-        val maxX = sensors.maxOf { it.maxX }
-
-        // draw(sensors, null, y)
-
-        var count = 0
-        for (x in minX.rangeTo(maxX)) {
-            if(sensors.any { it.inRange(x, y) } && sensors.none { it.beacon.x == x && it.beacon.y == y }) {
-                count++
-            }
-        }
-
-        return count
+        val map = parse(input)
+        if (y == 10) { draw(map) }
+        return map[y]!!.sumOf { (it.last - it.first) }
     }
 
     fun part2(input: List<String>, sx: Int = 20, sy: Int = 20): Long {
-        val sensors = input.map { parse(it) }.sortedBy { it.y }
-
-        for (y in 0..sy) {
-            val ranges = sensors.filter { it.xRanges.containsKey(y) }.map { it.xRanges[y]!! }.sortedBy { it.first }
-            var marker = max(ranges.first().last, 0)
-            ranges.drop(1).forEach { range ->
-                if (range.first <= marker) {
-                    marker = max(range.last, marker)
-                } else {
-                    return (range.first - 1) * 4000000L + y
-                }
+        val map = parse(input)
+        map.keys.sorted().filter { it in 0..sy }.forEach { y ->
+            if(map[y]!!.size > 1) {
+                return (map[y]!![1].first - 1) * 4000000L + y
             }
         }
 
@@ -95,16 +92,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
-    var elapsed = measureTimeMillis {
         check(part1(testInput, 10) == 26)
         check(part2(testInput) == 56000011L)
-    }
-    println("Completed test inputs in ${elapsed}ms")
 
     val input = readInput("Day15")
-    elapsed = measureTimeMillis {
         println(part1(input, 2000000))
         println(part2(input, 4000000, 4000000))
-    }
-    println("Completed Day15 inputs in ${elapsed}ms")
 }
